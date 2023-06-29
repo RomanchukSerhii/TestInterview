bf5cdc7426af8477547facba3ac804cbb359e3b5
unknown <serhiiromanchuk@gmail.com>
2023-06-29 23:10:27 +0300

    Add and implement AppDatabase class

--- app/src/main/java/com/example/testinterview/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/testinterview/data/database/AppDatabase.kt
@@ -1,4 +1,35 @@
 package com.example.testinterview.data.database
 
-abstract class AppDatabase {
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [TopicDbModel::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+        private val LOCK = Any()
+        private const val DB_NAME = "topic.db"
+
+        fun getInstance(application: Application): AppDatabase {
+            INSTANCE?.let {
+                return it
+            }
+
+            synchronized(LOCK) {
+                INSTANCE?.let {
+                    return it
+                }
+                val db = Room.databaseBuilder(
+                    application,
+                    AppDatabase::class.java,
+                    DB_NAME
+                ).build()
+                INSTANCE = db
+                return db
+            }
+        }
+    }
 }
