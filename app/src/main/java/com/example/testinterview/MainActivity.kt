9655b650f5f5c3a32c84e8a09a3eb9ea74db369b
unknown <serhiiromanchuk@gmail.com>
2023-06-30 23:02:30 +0300

    Add AppBar menu

--- app/src/main/java/com/example/testinterview/MainActivity.kt
+++ app/src/main/java/com/example/testinterview/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.testinterview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.fragment.app.Fragment
 import com.example.testinterview.databinding.ActivityMainBinding
+import com.example.testinterview.databinding.TopicItemBinding
+import com.example.testinterview.presentation.view.AddQuestionFragment
 import com.example.testinterview.presentation.view.ListTopicFragment
 import com.example.testinterview.presentation.view.MainFragment
 
@@ -15,7 +19,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        startMainFragment()
+        setMenuListeners()
+    }
+
+    private fun startMainFragment() {
         val fragment = MainFragment.newInstance()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    private fun setMenuListeners() {
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.add_question -> {
+                    val fragment = AddQuestionFragment.newInstance()
+                    startNewFragment(fragment)
+                    true
+                }
+                R.id.topic_list -> {
+                    val fragment = ListTopicFragment.newInstance()
+                    startNewFragment(fragment)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun startNewFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, fragment)
             .addToBackStack(null)
