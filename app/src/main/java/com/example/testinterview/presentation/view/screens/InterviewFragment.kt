abf42feeae57a7ca05459795c5c759fe43e922d2
unknown <serhiiromanchuk@gmail.com>
2023-07-06 15:09:28 +0300

    Implement onDeleteButtonClick login in InterviewFragment

--- app/src/main/java/com/example/testinterview/presentation/view/screens/InterviewFragment.kt
+++ app/src/main/java/com/example/testinterview/presentation/view/screens/InterviewFragment.kt
@@ -1,6 +1,8 @@
 package com.example.testinterview.presentation.view.screens
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +12,8 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.testinterview.App
 import com.example.testinterview.R
 import com.example.testinterview.databinding.FragmentInterviewBinding
+import com.example.testinterview.domain.model.Question
+import com.example.testinterview.presentation.view.adapters.QuestionItemActionListener
 import com.example.testinterview.presentation.view.adapters.QuestionListAdapter
 import com.example.testinterview.presentation.viewmodel.InterviewViewModel
 import com.example.testinterview.presentation.viewmodel.ViewModelFactory
@@ -34,7 +38,8 @@ class InterviewFragment : Fragment() {
     }
 
     private val questionListAdapter by lazy {
-        QuestionListAdapter()
+        val actionListener = getQuestionItemActionListener()
+        QuestionListAdapter(actionListener)
     }
 
     override fun onAttach(context: Context) {
@@ -59,6 +64,31 @@ class InterviewFragment : Fragment() {
     private fun observeViewModel() {
         viewModel.questionListLD.observe(viewLifecycleOwner) {
             questionListAdapter.submitList(it)
+        }
+    }
+
+    private fun getQuestionItemActionListener(): QuestionItemActionListener {
+        return object : QuestionItemActionListener {
+            override fun onDeleteButtonClick(question: Question) {
+                val listener = DialogInterface.OnClickListener { _, which ->
+                    when(which) {
+                        DialogInterface.BUTTON_POSITIVE -> viewModel.deleteQuestion(question)
+                    }
+
+                }
+                AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.delete_question)
+                    .setMessage(R.string.delete_question_message)
+                    .setNegativeButton(R.string.no, listener)
+                    .setPositiveButton(R.string.confirm, listener)
+                    .create()
+                    .show()
+
+            }
+
+            override fun onEditButtonClick(question: Question) {
+                viewModel.editQuestion(question)
+            }
         }
     }
 
--- app/src/main/java/com/example/testinterview/presentation/viewmodel/InterviewViewModel.kt
+++ app/src/main/java/com/example/testinterview/presentation/viewmodel/InterviewViewModel.kt
@@ -1,11 +1,30 @@
 package com.example.testinterview.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.testinterview.domain.model.Question
+import com.example.testinterview.domain.usecase.DeleteQuestionUseCase
+import com.example.testinterview.domain.usecase.EditQuestionUseCase
 import com.example.testinterview.domain.usecase.GetQuestionListUseCase
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class InterviewViewModel @Inject constructor(
-    private val getQuestionListUseCase: GetQuestionListUseCase
+    private val getQuestionListUseCase: GetQuestionListUseCase,
+    private val deleteQuestionUseCase: DeleteQuestionUseCase,
+    private val editQuestionUseCase: EditQuestionUseCase
 ) : ViewModel() {
     val questionListLD = getQuestionListUseCase.invoke()
+
+    fun deleteQuestion(question: Question) {
+        viewModelScope.launch {
+            deleteQuestionUseCase.invoke(questionId = question.id)
+        }
+    }
+
+    fun editQuestion(question: Question) {
+        viewModelScope.launch {
+            editQuestionUseCase.invoke(question)
+        }
+    }
 }
